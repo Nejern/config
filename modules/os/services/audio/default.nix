19264bb7e4ad5b71911dd26ac2397f5b365da654
Oleg Nikitin <5qub6pfp@duck.com>
2024-04-14 12:17:32 +0300

    modules(os): update audio service

--- modules/os/services/audio/default.nix
+++ modules/os/services/audio/default.nix
@@ -15,14 +15,16 @@
       pulse.enable = true;
       jack.enable = true;
       wireplumber.enable = true;
-      #extraConfig.pipewire."92-low-latency" = {
-      #  "context.properties" = {
-      #    "default.clock.rate" = 44100;
-      #    "default.clock.quantum" = 512;
-      #    "default.clock.min-quantum" = 512;
-      #    "default.clock.max-quantum" = 512;
-      #  };
-      #};
+      extraConfig.pipewire."92-low-latency" = {
+        context.properties = {
+          default.clock = {
+            rate = 48000;
+            quantum = 32;
+            min-quantum = 32;
+            max-quantum = 32;
+          };
+        };
+      };
     };
   };
 }
