26ca5ba9ee73fa74bb20052622b2ee576c0d56dc
Oleg Nikitin <5qub6pfp@duck.com>
2024-04-17 19:43:23 +0300

    modules(home): update zsh shell

--- modules/home/programs/shells/zsh/default.nix
+++ modules/home/programs/shells/zsh/default.nix
@@ -29,19 +29,29 @@
           src = ./p10k-config;
           file = "p10k.zsh";
         }
+        {
+          name = "zsh-nix-shell";
+          file = "nix-shell.plugin.zsh";
+          src = pkgs.fetchFromGitHub {
+            owner = "chisui";
+            repo = "zsh-nix-shell";
+            rev = "v0.8.0";
+            sha256 = "1lzrn0n4fxfcgg65v0qhnj7wnybybqzs4adz7xsrkgmcsr0ii8b7";
+          };
+        }
       ];
 
       # Oh My Zsh
-      oh-my-zsh = {
-        enable = true;
-        plugins = [
-          "rust"
-          "colored-man-pages"
-          "vi-mode"
-          "rust"
-          "docker-compose"
-        ];
-      };
+      #oh-my-zsh = {
+      #  enable = true;
+      #  plugins = [
+      #    "rust"
+      #    "colored-man-pages"
+      #    "vi-mode"
+      #    "rust"
+      #    "docker-compose"
+      #  ];
+      #};
     };
   };
 }
