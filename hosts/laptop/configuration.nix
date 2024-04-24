ffc2c2152a500083fc1aec5c37fab79c98db5583
Oleg Nikitin <5qub6pfp@duck.com>
2024-04-25 01:10:52 +0300

    laptop(os): add nix-ld

--- hosts/laptop/configuration.nix
+++ hosts/laptop/configuration.nix
@@ -75,6 +75,43 @@
     driSupport32Bit = true;
   };
 
+  programs.nix-ld.enable = true;
+  # "minimum" amount of libraries needed for most games to run without steam-run
+  programs.nix-ld.libraries = with pkgs; [
+    # common requirement for several games
+    stdenv.cc.cc.lib
+
+    glm
+    assimp
+    stb
+    glfw
+
+    # from https://github.com/NixOS/nixpkgs/blob/nixos-23.05/pkgs/games/steam/fhsenv.nix#L72-L79
+    xorg.libXcomposite
+    xorg.libXtst
+    xorg.libXrandr
+    xorg.libXext
+    xorg.libX11
+    xorg.libXfixes
+    libGL
+    libva
+
+    # from https://github.com/NixOS/nixpkgs/blob/nixos-23.05/pkgs/games/steam/fhsenv.nix#L124-L136
+    fontconfig
+    freetype
+    xorg.libXt
+    xorg.libXmu
+    libogg
+    libvorbis
+    SDL
+    SDL2_image
+    glew110
+    libdrm
+    libidn
+    tbb
+    zlib
+  ];
+
   # Load nvidia driver for Xorg and Wayland
   services.xserver.videoDrivers = [ "nvidia" ];
   hardware.nvidia = {
