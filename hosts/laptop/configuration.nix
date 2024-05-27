e7b6cfb6f1e52e4e358453149fe9d208529bf9ff
Oleg Nikitin <5qub6pfp@duck.com>
2024-05-27 22:20:25 +0300

    laptop(os): nix-ld

--- hosts/laptop/configuration.nix
+++ hosts/laptop/configuration.nix
@@ -83,6 +83,41 @@
     driSupport32Bit = true;
   };
 
+  programs.nix-ld.enable = true;
+  programs.nix-ld.libraries = with pkgs; [
+    stdenv.cc.cc.lib
+
+    glm
+    assimp
+    stb
+    glfw
+
+    # from https://github.com/NixOS/nixpkgs/blob/nixos-23.05/pkgs/games/steam/fhsenv.nix#L72-L79
+    xorg.libXcomposite
+    xorg.libXtst
+    xorg.libXrandr
+    xorg.libXext
+    xorg.libX11
+    xorg.libXfixes
+    libGL
+    libva
+
+    # from https://github.com/NixOS/nixpkgs/blob/nixos-23.05/pkgs/games/steam/fhsenv.nix#L124-L136
+    fontconfig
+    freetype
+    xorg.libXt
+    xorg.libXmu
+    libogg
+    libvorbis
+    SDL
+    SDL2_image
+    glew110
+    libdrm
+    libidn
+    tbb
+    zlib
+  ];
+
   specialisation = {
     gamemode.configuration = {
       system.nixos.tags = [ "gamemode" ];
@@ -127,43 +162,6 @@
         # Optionally, you may need to select the appropriate driver version for your specific GPU.
         #package = config.boot.kernelPackages.nvidiaPackages.stable;
       };
-
-      programs.nix-ld.enable = true;
-      # "minimum" amount of libraries needed for most games to run without steam-run
-      programs.nix-ld.libraries = with pkgs; [
-        # common requirement for several games
-        stdenv.cc.cc.lib
-
-        glm
-        assimp
-        stb
-        glfw
-
-        # from https://github.com/NixOS/nixpkgs/blob/nixos-23.05/pkgs/games/steam/fhsenv.nix#L72-L79
-        xorg.libXcomposite
-        xorg.libXtst
-        xorg.libXrandr
-        xorg.libXext
-        xorg.libX11
-        xorg.libXfixes
-        libGL
-        libva
-
-        # from https://github.com/NixOS/nixpkgs/blob/nixos-23.05/pkgs/games/steam/fhsenv.nix#L124-L136
-        fontconfig
-        freetype
-        xorg.libXt
-        xorg.libXmu
-        libogg
-        libvorbis
-        SDL
-        SDL2_image
-        glew110
-        libdrm
-        libidn
-        tbb
-        zlib
-      ];
       # Steam
       programs.steam = {
         enable = true;
