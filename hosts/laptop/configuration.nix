f259f245ff6325b97ee462002d306a13b9283933
Oleg Nikitin <5qub6pfp@duck.com>
2024-05-27 16:29:59 +0300

    laptop(os): gamemode

--- hosts/laptop/configuration.nix
+++ hosts/laptop/configuration.nix
@@ -31,16 +31,6 @@
   # Shell
   programs.zsh.enable = true;
 
-  # Steam
-  programs.steam = {
-    enable = true;
-    gamescopeSession.enable = true;
-  };
-  # Gamemode
-  programs.gamemode = {
-    enable = true;
-  };
-
   # Networking
   networking.hostName = hostname;
   networking.networkmanager = {
@@ -72,17 +62,6 @@
     size = 16 * 1024;
   }];
 
-  #specialisation = {
-  #  on-the-go.configuration = {
-  #    system.nixos.tags = [ "on-the-go" ];
-  #    hardware.nvidia = {
-  #      prime.offload.enable = lib.mkForce true;
-  #      prime.offload.enableOffloadCmd = lib.mkForce true;
-  #      prime.sync.enable = lib.mkForce false;
-  #    };
-  #  };
-  #};
-
   # Set your time zone.
   time.timeZone = "Europe/Moscow";
   # Windows time support
@@ -104,83 +83,97 @@
     driSupport32Bit = true;
   };
 
-  programs.nix-ld.enable = true;
-  # "minimum" amount of libraries needed for most games to run without steam-run
-  programs.nix-ld.libraries = with pkgs; [
-    # common requirement for several games
-    stdenv.cc.cc.lib
+  specialisation = {
+    gamemode.configuration = {
+      system.nixos.tags = [ "gamemode" ];
+      # Load nvidia driver for Xorg and Wayland
+      services.xserver.videoDrivers = [ "nvidia" ];
+      hardware.nvidia = {
+        prime = {
+          offload = {
+            enable = true;
+            enableOffloadCmd = true;
+          };
+          # Make sure to use the correct Bus ID values for your system!
+          amdgpuBusId = "PCI:5:0:0";
+          nvidiaBusId = "PCI:1:0:0";
+        };
+        # Modesetting is required.
+        modesetting.enable = true;
 
-    glm
-    assimp
-    stb
-    glfw
+        # Nvidia power management. Experimental, and can cause sleep/suspend to fail.
+        # Enable this if you have graphical corruption issues or application crashes after waking
+        # up from sleep. This fixes it by saving the entire VRAM memory to /tmp/ instead 
+        # of just the bare essentials.
+        powerManagement.enable = false;
 
-    # from https://github.com/NixOS/nixpkgs/blob/nixos-23.05/pkgs/games/steam/fhsenv.nix#L72-L79
-    xorg.libXcomposite
-    xorg.libXtst
-    xorg.libXrandr
-    xorg.libXext
-    xorg.libX11
-    xorg.libXfixes
-    libGL
-    libva
+        # Fine-grained power management. Turns off GPU when not in use.
+        # Experimental and only works on modern Nvidia GPUs (Turing or newer).
+        powerManagement.finegrained = false;
 
-    # from https://github.com/NixOS/nixpkgs/blob/nixos-23.05/pkgs/games/steam/fhsenv.nix#L124-L136
-    fontconfig
-    freetype
-    xorg.libXt
-    xorg.libXmu
-    libogg
-    libvorbis
-    SDL
-    SDL2_image
-    glew110
-    libdrm
-    libidn
-    tbb
-    zlib
-  ];
+        # Use the NVidia open source kernel module (not to be confused with the
+        # independent third-party "nouveau" open source driver).
+        # Support is limited to the Turing and later architectures. Full list of 
+        # supported GPUs is at: 
+        # https://github.com/NVIDIA/open-gpu-kernel-modules#compatible-gpus 
+        # Only available from driver 515.43.04+
+        # Currently alpha-quality/buggy, so false is currently the recommended setting.
+        open = false;
 
-  # Load nvidia driver for Xorg and Wayland
-  services.xserver.videoDrivers = [ "nvidia" ];
-  hardware.nvidia = {
-    prime = {
-      offload = {
-        enable = true;
-        enableOffloadCmd = true;
+        # Enable the Nvidia settings menu,
+        # accessible via `nvidia-settings`.
+        nvidiaSettings = true;
+
+        # Optionally, you may need to select the appropriate driver version for your specific GPU.
+        #package = config.boot.kernelPackages.nvidiaPackages.stable;
       };
-      # Make sure to use the correct Bus ID values for your system!
-      amdgpuBusId = "PCI:5:0:0";
-      nvidiaBusId = "PCI:1:0:0";
+
+      programs.nix-ld.enable = true;
+      # "minimum" amount of libraries needed for most games to run without steam-run
+      programs.nix-ld.libraries = with pkgs; [
+        # common requirement for several games
+        stdenv.cc.cc.lib
+
+        glm
+        assimp
+        stb
+        glfw
+
+        # from https://github.com/NixOS/nixpkgs/blob/nixos-23.05/pkgs/games/steam/fhsenv.nix#L72-L79
+        xorg.libXcomposite
+        xorg.libXtst
+        xorg.libXrandr
+        xorg.libXext
+        xorg.libX11
+        xorg.libXfixes
+        libGL
+        libva
+
+        # from https://github.com/NixOS/nixpkgs/blob/nixos-23.05/pkgs/games/steam/fhsenv.nix#L124-L136
+        fontconfig
+        freetype
+        xorg.libXt
+        xorg.libXmu
+        libogg
+        libvorbis
+        SDL
+        SDL2_image
+        glew110
+        libdrm
+        libidn
+        tbb
+        zlib
+      ];
+      # Steam
+      programs.steam = {
+        enable = true;
+        gamescopeSession.enable = true;
+      };
+      # Gamemode
+      programs.gamemode = {
+        enable = true;
+      };
     };
-    # Modesetting is required.
-    modesetting.enable = true;
-
-    # Nvidia power management. Experimental, and can cause sleep/suspend to fail.
-    # Enable this if you have graphical corruption issues or application crashes after waking
-    # up from sleep. This fixes it by saving the entire VRAM memory to /tmp/ instead 
-    # of just the bare essentials.
-    powerManagement.enable = false;
-
-    # Fine-grained power management. Turns off GPU when not in use.
-    # Experimental and only works on modern Nvidia GPUs (Turing or newer).
-    powerManagement.finegrained = false;
-
-    # Use the NVidia open source kernel module (not to be confused with the
-    # independent third-party "nouveau" open source driver).
-    # Support is limited to the Turing and later architectures. Full list of 
-    # supported GPUs is at: 
-    # https://github.com/NVIDIA/open-gpu-kernel-modules#compatible-gpus 
-    # Only available from driver 515.43.04+
-    # Currently alpha-quality/buggy, so false is currently the recommended setting.
-    open = false;
-
-    # Enable the Nvidia settings menu,
-    # accessible via `nvidia-settings`.
-    nvidiaSettings = true;
-
-    # Optionally, you may need to select the appropriate driver version for your specific GPU.
-    package = config.boot.kernelPackages.nvidiaPackages.stable;
   };
 
   system.stateVersion = "23.11"; # Don't touch this.
