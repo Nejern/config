af53b7cdf2afd1e12a28d3897b72b6225bd28d25
Oleg Nikitin <oenikitin@zlp.ooo>
2024-06-22 12:56:27 +0300

    modules(home): update gtk gruvbox theme

--- modules/home/gtk/themes/gruvbox/theme.nix
+++ modules/home/gtk/themes/gruvbox/theme.nix
@@ -8,13 +8,13 @@
 }:
 stdenvNoCC.mkDerivation {
   pname = "gruvbox-gtk-theme";
-  version = "unstable-2024-06-16";
+  version = "unstable-2024-06-18";
 
   src = fetchFromGitHub {
     owner = "Fausto-Korpsvart";
     repo = "Gruvbox-GTK-Theme";
-    rev = "de4e837044f99286b6a079d7362b809e8a17f404";
-    hash = "sha256-VFm9LpvQXkNB7CYa/DpJeICfdwgf2oXNfH1W2TFg5y0=";
+    rev = "5e99fc6c2b30b0489065254240f15cd6d401eb0b";
+    hash = "sha256-wUBOR9CjjD2MwT3PdLw/mccVLYhwVCaTJ+fe2V2HhnE=";
   };
 
   propagatedUserEnvPkgs = [
