70240aaab2613482fa0c4516b5296408a2221cb1
Oleg Nikitin <oenikitin@zlp.ooo>
2024-07-21 23:58:42 +0300

    modules(home): update gtk gruvbox theme

--- modules/home/gtk/themes/gruvbox/theme.nix
+++ modules/home/gtk/themes/gruvbox/theme.nix
@@ -1,45 +1,52 @@
 { lib
 , stdenvNoCC
 , fetchFromGitHub
+, gnome
+, sassc
 , gnome-themes-extra
 , gtk-engine-murrine
-, sassc
-, gnome
+, colorVariants ? [ ] # default: install all icons
 }:
-stdenvNoCC.mkDerivation {
+let
   pname = "gruvbox-gtk-theme";
+  colorVariantList = [
+    "dark"
+    "light"
+  ];
+in
+stdenvNoCC.mkDerivation {
+  inherit pname;
   version = "unstable-2024-06-18";
 
   src = fetchFromGitHub {
     owner = "Fausto-Korpsvart";
     repo = "Gruvbox-GTK-Theme";
-    rev = "5e99fc6c2b30b0489065254240f15cd6d401eb0b";
-    hash = "sha256-wUBOR9CjjD2MwT3PdLw/mccVLYhwVCaTJ+fe2V2HhnE=";
+    rev = "84c8a179a7aa6b8fa6672c17f98ea987455db5f3";
+    hash = "sha256-Hep/V8dN8Wdm2U8HXZBuJsAazDkLggk2xRkxRoAeSkA=";
   };
 
-  propagatedUserEnvPkgs = [
-    gtk-engine-murrine
-  ];
+  propagatedUserEnvPkgs = [ gtk-engine-murrine ];
 
-  buildInputs = [
-    gnome-themes-extra
-    gnome.gnome-shell
-    sassc
-  ];
+  nativeBuildInputs = [ gnome.gnome-shell sassc ];
+  buildInputs = [ gnome-themes-extra ];
 
   dontBuild = true;
+
+  postPatch = ''
+    patchShebangs themes/install.sh
+  '';
 
   installPhase = ''
     runHook preInstall
     mkdir -p $out/share/themes
-    bash themes/install.sh -n Gruvbox -d $out/share/themes -t all
+    bash themes/install.sh -n Gruvbox -c ${lib.concatStringsSep " " (if colorVariants != [] then colorVariants else colorVariantList)} -d $out/share/themes -t all --tweaks macos
     runHook postInstall
   '';
 
   meta = with lib; {
     description = "Gtk theme based on the Gruvbox colour pallete";
-    homepage = "https://www.pling.com/p/1681313/";
-    license = licenses.gpl3Only;
+    homepage = "https://github.com/Fausto-Korpsvart/Gruvbox-GTK-Theme";
+    license = lib.licenses.gpl3Plus;
     platforms = platforms.unix;
     maintainers = [ maintainers.math-42 ];
   };
