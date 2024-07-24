1fd21091043cd4bb452c55bf1ce8d50c367dee0c
Oleg Nikitin <oenikitin@zlp.ooo>
2024-07-24 18:30:59 +0300

    modules(home): update hyprland

--- modules/home/desktop/hyprland/default.nix
+++ modules/home/desktop/hyprland/default.nix
@@ -173,5 +173,50 @@
             10)
         );
     };
+    wayland.windowManager.hyprland.extraConfig = ''
+      # Submaps
+      $submap_resize = 🍆 resize
+      $submap_audio = 🔊 audio
+      $submap_exit = 🏁 exit
+
+      bind = $mainMod$shift, a, submap, $submap_audio
+      bind = $mainMod$shift, r, submap, $submap_resize
+      bind = $mainMod$shift, e, submap, $submap_exit
+
+      submap = $submap_audio
+        binde = , k, exec, wpctl set-volume @DEFAULT_SINK@ 1%+
+        binde = , j, exec, wpctl set-volume @DEFAULT_SINK@ 1%-
+
+        bind = , return, submap, reset
+        bind = , escape, submap, reset
+      submap = reset
+
+      submap = $submap_resize
+        binde = , h, resizeactive, -15 0
+        binde = , l, resizeactive, 15 0
+        binde = , k, resizeactive, 0 -15
+        binde = , j, resizeactive, 0 15
+
+        bind  = , escape, submap, reset
+        bind  = , return, submap, reset
+      submap = reset
+
+      submap = $submap_exit
+        bind = , e, exit,
+        #bind = , l, exec, exit-wm lock
+        #bind = , l, submap, reset
+        bind = , s, exec, systemctl -i suspend
+        bind = , s, submap, reset
+        bind = , r, exec, systemctl -i reboot
+        bind = , r, submap, reset
+        bind = , h, exec, systemctl -i hibernate
+        bind = , h, submap, reset
+        bind = , p, exec, systemctl -i poweroff
+        bind = , p, submap, reset
+
+        bind = , return, submap, reset
+        bind = , escape, submap, reset
+      submap = reset
+    '';
   };
 }
