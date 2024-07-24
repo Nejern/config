3f97a1307481aa7bd565f2069027e61a5ed28090
Oleg Nikitin <oenikitin@zlp.ooo>
2024-07-24 14:21:03 +0300

    modules(os): update hyprland

--- modules/os/manager/desktop/hyprland/default.nix
+++ modules/os/manager/desktop/hyprland/default.nix
@@ -5,6 +5,9 @@
   };
 
   config = lib.mkIf config.module.manager.desktop.hyprland.enable {
+    environment.systemPackages = with pkgs; [
+      wl-clipboard
+    ];
     environment.sessionVariables = {
       NIXOS_OZONE_WL = "1";
     };
