77c88e7456096111a4da96bd016d0aaa2222327a
Oleg Nikitin <oenikitin@zlp.ooo>
2024-07-26 18:08:18 +0300

    laptop(os): add bluetooth

--- hosts/laptop/configuration.nix
+++ hosts/laptop/configuration.nix
@@ -10,6 +10,7 @@
       podman.enable = true;
     };
     service = {
+      udev.rules.enable = true;
       audio.enable = true;
       tlp.enable = true;
       syncthing.enable = true;
@@ -81,6 +82,14 @@
   environment.sessionVariables = {
     FLAKE = "/home/${username}/nixos";
   };
+
+  hardware.bluetooth.enable = true;
+  #systemd.user.services.mpris-proxy = {
+  #  description = "Mpris proxy";
+  #  after = [ "network.target" "sound.target" ];
+  #  wantedBy = [ "default.target" ];
+  #  serviceConfig.ExecStart = "${pkgs.bluez}/bin/mpris-proxy";
+  #};
 
   # OpenGL
   hardware.opengl = {
--- modules/os/services/audio/default.nix
+++ modules/os/services/audio/default.nix
@@ -13,7 +13,17 @@
       alsa.support32Bit = true;
       pulse.enable = true;
       jack.enable = true;
-      wireplumber.enable = true;
+      wireplumber = {
+        enable = true;
+        #extraConfig.bluetoothEnhancements = {
+        #  "monitor.bluez.properties" = {
+        #    "bluez5.enable-sbc-xq" = true;
+        #    "bluez5.enable-msbc" = true;
+        #    "bluez5.enable-hw-volume" = true;
+        #    "bluez5.roles" = [ "hsp_hs" "hsp_ag" "hfp_hf" "hfp_ag" ];
+        #  };
+        #};
+      };
       extraConfig.pipewire."92-low-latency" = {
         context.properties = {
           default.clock = {
