fb120ba44a86f729990a442654a52baa7ab34dad
Oleg Nikitin <oenikitin@zlp.ooo>
2024-07-26 18:07:11 +0300

    modules(os): add udev service

--- modules/os/services/default.nix
+++ modules/os/services/default.nix
@@ -3,9 +3,6 @@
     ./tlp
     ./audio
     ./syncthing
+    ./udev
   ];
-
-  module.service.audio.enable = lib.mkDefault false;
-  module.service.tlp.enable = lib.mkDefault false;
-  module.service.syncthing.enable = lib.mkDefault false;
 }
