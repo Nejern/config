3d48dc2ad67d9c6e7dab9e1114f4ce5bfca964aa
Oleg Nikitin <oenikitin@zlp.ooo>
2024-08-03 21:42:30 +0300

    modules(home): update hyprland

--- modules/home/desktop/hyprland/default.nix
+++ modules/home/desktop/hyprland/default.nix
@@ -185,8 +185,7 @@
           "$ctrl $alt $shift, DELETE, exec, systemctl -i reboot"
 
           # Keyboard brightness
-          #", keyboard_brightness_up_shortcut, exec, ~/.config/hypr/scripts/kbbacklight --inc"
-          #", keyboard_brightness_down_shortcut, exec, ~/.config/hypr/scripts/kbbacklight --dec"
+          ", XF86KbdBrightness, exec, ~/.config/hypr/scripts/kbbacklight --inc"
 
           # Clipboard history
           "SUPER, V, exec, cliphist list | wofi --dmenu | cliphist decode | wl-copy"
@@ -212,50 +211,50 @@
             10)
         );
     };
-    #wayland.windowManager.hyprland.extraConfig = ''
-    #  # Submaps
-    #  $submap_resize = 🍆 resize
-    #  $submap_audio = 🔊 audio
-    #  $submap_exit = 🏁 exit
+    wayland.windowManager.hyprland.extraConfig = ''
+      # Submaps
+      $submap_resize = 🍆 resize
+      $submap_audio = 🔊 audio
+      $submap_exit = 🏁 exit
 
-    #  bind = $mainMod$shift, a, submap, $submap_audio
-    #  bind = $mainMod$shift, r, submap, $submap_resize
-    #  bind = $mainMod$shift, e, submap, $submap_exit
+      bind = $mainMod$shift, a, submap, $submap_audio
+      bind = $mainMod$shift, r, submap, $submap_resize
+      bind = $mainMod$shift, e, submap, $submap_exit
 
-    #  submap = $submap_audio
-    #    binde = , k, exec, wpctl set-volume @DEFAULT_SINK@ 1%+
-    #    binde = , j, exec, wpctl set-volume @DEFAULT_SINK@ 1%-
+      submap = $submap_audio
+        binde = , k, exec, ~/.config/hypr/scripts/volume --inc
+        binde = , j, exec, ~/.config/hypr/scripts/volume --dec
 
-    #    bind = , return, submap, reset
-    #    bind = , escape, submap, reset
-    #  submap = reset
+        bind = , return, submap, reset
+        bind = , escape, submap, reset
+      submap = reset
 
-    #  submap = $submap_resize
-    #    binde = , h, resizeactive, -15 0
-    #    binde = , l, resizeactive, 15 0
-    #    binde = , k, resizeactive, 0 -15
-    #    binde = , j, resizeactive, 0 15
+      submap = $submap_resize
+        binde = , h, resizeactive, -15 0
+        binde = , l, resizeactive, 15 0
+        binde = , k, resizeactive, 0 -15
+        binde = , j, resizeactive, 0 15
 
-    #    bind  = , escape, submap, reset
-    #    bind  = , return, submap, reset
-    #  submap = reset
+        bind  = , escape, submap, reset
+        bind  = , return, submap, reset
+      submap = reset
 
-    #  submap = $submap_exit
-    #    bind = , e, exit,
-    #    #bind = , l, exec, exit-wm lock
-    #    #bind = , l, submap, reset
-    #    bind = , s, exec, systemctl -i suspend
-    #    bind = , s, submap, reset
-    #    bind = , r, exec, systemctl -i reboot
-    #    bind = , r, submap, reset
-    #    bind = , h, exec, systemctl -i hibernate
-    #    bind = , h, submap, reset
-    #    bind = , p, exec, systemctl -i poweroff
-    #    bind = , p, submap, reset
+      submap = $submap_exit
+        bind = , e, exit,
+        #bind = , l, exec, exit-wm lock
+        #bind = , l, submap, reset
+        bind = , s, exec, systemctl -i suspend
+        bind = , s, submap, reset
+        bind = , r, exec, systemctl -i reboot
+        bind = , r, submap, reset
+        bind = , h, exec, systemctl -i hibernate
+        bind = , h, submap, reset
+        bind = , p, exec, systemctl -i poweroff
+        bind = , p, submap, reset
 
-    #    bind = , return, submap, reset
-    #    bind = , escape, submap, reset
-    #  submap = reset
-    #'';
+        bind = , return, submap, reset
+        bind = , escape, submap, reset
+      submap = reset
+    '';
   };
 }
