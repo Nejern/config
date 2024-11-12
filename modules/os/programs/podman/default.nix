c5fb4fef8381892d4a8d574cc6128336b85bc5d8
Oleg Nikitin <oenikitin@zlp.ooo>
2024-11-12 17:01:38 +0300

    module(os): update podman

--- modules/os/programs/podman/default.nix
+++ modules/os/programs/podman/default.nix
@@ -17,17 +17,25 @@
         };
       };
 
+      # run container as systemd example
+      #oci-containers.containers = {
+      #  container-name = {
+      #    image = "container-image";
+      #    autoStart = true;
+      #    ports = [ "127.0.0.1:1234:1234" ];
+      #  };
+      #};
       oci-containers.backend = "podman";
       podman = {
         enable = true;
         # Create a `docker` alias for podman, to use it as a drop-in replacement
-        dockerCompat = true;
+        dockerCompat = false;
         # Required for containers under podman-compose to be able to talk to each other.
         defaultNetwork.settings.dns_enabled = true;
       };
     };
-    environment.systemPackages = with pkgs; [ docker-compose ];
-    environment.extraInit = ''
+    environment.systemPackages = with pkgs; [ podman-compose podman-tui dive buildah ];
+    environment.extraInit = lib.mkIf config.virtualisation.podman.dockerCompat ''
       if [ -z "$DOCKER_HOST" -a -n "$XDG_RUNTIME_DIR" ]; then
         export DOCKER_HOST="unix://$XDG_RUNTIME_DIR/podman/podman.sock"
       fi
